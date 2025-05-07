de5d2b5380957aaabe45e4894124495de34fe0de
Bill <bill@matranx.com.tw>
2025-05-07 15:15:37 +0800

    Update LOLdataset.py

--- data/LOLdataset.py
+++ data/LOLdataset.py
@@ -111,6 +111,42 @@ class LOLv2SynDatasetFromFolder(data.Dataset):
         return 900
 
 
+class CECDatasetFromFolder(data.Dataset):
+    def __init__(self, data_dir, transform=None):
+        super(CECDatasetFromFolder, self).__init__()
+        self.data_dir = data_dir
+        self.transform = transform
+
+    def __getitem__(self, index):
+        folder = self.data_dir + '/corrupted'
+        folder2 = self.data_dir + '/clean'
+        data_filenames = [join(folder, x) for x in listdir(folder) if is_image_file(x)]
+        data_filenames2 = [join(folder2, x) for x in listdir(folder2) if is_image_file(x)]
+        
+        im1 = load_img(data_filenames[index])
+        im2 = load_img(data_filenames2[index])
+        _, file1 = os.path.split(data_filenames[index])
+        _, file2 = os.path.split(data_filenames2[index])
+        
+        seed = random.randint(1, 1000000)
+        seed = np.random.randint(seed)
+        
+        if self.transform:
+            random.seed(seed)
+            torch.manual_seed(seed)
+            im1 = self.transform(im1)
+            random.seed(seed)
+            torch.manual_seed(seed)
+            im2 = self.transform(im2)
+        
+        return im1, im2, file1, file2
+
+    def __len__(self):
+        folder = self.data_dir + '/corrupted'
+        data_filenames = [join(folder, x) for x in listdir(folder) if is_image_file(x)]
+        return len(data_filenames)
+
+
 
     
 
